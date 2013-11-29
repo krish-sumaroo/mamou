6c8ec31967f93fa793b5eedbe36024d0f1d85214
Krish Sumaroo <ksumaroo@administrators-Mac-mini.local>
2013-11-29 16:37:20 -0500

    products page

---
+++ application/views/products/products.php
@@ -0,0 +1,14 @@
+<div class="col-md-7 col-md-offset-1 list">
+  <div class="panel panel-info">
+      <div class="panel-heading">List</div>
+        <div class="panel-body">
+        	<div id="list">
+        		<a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+			    <a href="#" class="list-group-item">Morbi leo risus</a>
+			    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+			    <a href="#" class="list-group-item">Vestibulum at eros</a>
+        	</div>	
+        </div>
+      </div>
+  </div>
+</div>                      
