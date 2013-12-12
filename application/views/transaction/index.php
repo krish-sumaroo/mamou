731e24097b77f65dd49d10e7d99f6bd458392849
Krish Sumaroo <ksumaroo@administrators-Mac-mini.local>
2013-12-12 18:11:28 -0500

    table design

--- application/views/transaction/index.php
+++ application/views/transaction/index.php
@@ -1,10 +1,22 @@
-<ul id="transacList" class="pagination">
-    <li id="trHeader">
-        <span class="trProductName">Name</span>
-        <span class="trProductModel">Model</span>
-        <span class="trProductSize">Size</span>
-        <span class="trProductQty">Qty</span>
-        <span class="trProductTotal">Total</span>
-        <span class="trStatus">Status</span>
-    </li>
-</ul>
+<table class="table table-hover" >
+    <thead>
+        <th>Name</th>
+        <td>Model</td>
+        <td>Size</td>
+        <td>Qty</td>
+        <td>Total</td>
+        <td>Status</td>
+    </thead>
+    
+    <tbody id="tblContent">
+        <tr>
+            <td>Name 1</td>
+            <td>Model 1</td>
+            <td>Size 1</td>
+            <td><input type="text" class="inpQty" /></td>
+            <td><input type="number" class="inpTotal" /></td>
+            <td>icons here</td>
+        </tr>
+    </tbody>
+    
+</table>
